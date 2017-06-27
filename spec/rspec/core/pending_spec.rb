874502c2255ab6f508232d0784d5bfd87042a40f
Devon Estes <devon.c.estes@gmail.com>
2017-06-27 19:57:32 +0200

    Add example of unexpected behavior
    When I try and make an entire test pending using `xdescribe`, code that
    is put in any `before(:all)` blocks are still executed. This seems to
    me like it should not happen since I'm saying that I want to skip that
    whole set of tests. In my eyes as a user, I wouldn't expect anything in
    that block to execute if it's being skipped.
    
    I think this is a bug, but I'm not 100% sure. At the very least, it
    seems surprising...

--- spec/rspec/core/pending_spec.rb
+++ spec/rspec/core/pending_spec.rb
@@ -1,4 +1,8 @@
-RSpec.describe RSpec::Core::Pending do
+RSpec.xdescribe RSpec::Core::Pending do
+  before(:all) do
+    raise NotImplementedError, 'why is this raising?'
+  end
+
   it 'only defines methods that are part of the DSL' do
     expect(RSpec::Core::Pending.instance_methods(false).map(&:to_sym)).to \
       match_array([:pending, :skip])
